b7661e584f8904c2094843e87287ef21f8232ed2
Robin Green <robin@designingreen.co.uk>
2022-01-28 18:17:52 +0000

    Maths/JavaScript initial, start of layout

--- frontend/src/components/status/index.jsx
+++ frontend/src/components/status/index.jsx
@@ -7,19 +7,38 @@ class Status extends React.Component {
 
     render() {
         console.dir(this.props);
-        if (this.props.resp.neofs === undefined){
+        if ((this.props.resp.neofs === undefined) || 
+            (this.props.resp.nep17 === undefined) || 
+            (this.props.resp.nep17.GAS === undefined) || 
+            (this.props.resp.nep17.GAS.meta === undefined) || 
+            (this.props.resp.nep17.NEO === undefined)) {
             console.log("Waiting...");
             return(<div>Waiting...</div>)
         }
+
+        // Don't think you need Number()
+        var b = Number((this.props.resp.neofs.balance));
+        var p = Number((this.props.resp.neofs.precision));
+        var m = Math.pow(10,p);
+        var g = Number(b/m).toFixed(2);
+
+        var gs = Number((this.props.resp.nep17.GAS.amount));
+        var dp = Number((this.props.resp.nep17.GAS.meta.decimals));
+        var ms = Math.pow(10,dp);
+        var gb = Number(gs/ms).toFixed(2); // Check that this is 10 to the power... 
+
         return (
-            <div className="result" id="result">
-                <div className="molBlockJSON">
-                    <ul>
-                        <li>Wallet: {this.props.resp.address}</li>
-                        <li>NeoFS: {this.props.resp.neofs.balance}</li>
-                    </ul>
+            <>
+                <div className="d-flex">
+                    <div class="molBlockData">Wallet: {this.props.resp.address}</div>
                 </div>
-            </div>
+                <div className="d-flex justify-content-between">
+                    
+                    <div class="molBlockData"><span class="utUnit">NeoFS</span><span class="utNum">{g}</span></div>
+                    <div class="molBlockData"><span class="utUnit">GAS</span><span class="utNum">{gb}</span></div>
+                    <div class="molBlockData"><span class="utUnit">NEO</span><span class="utNum">{this.props.resp.nep17.NEO.amount}</span></div>
+                </div>
+            </>
         );
     }
 }
