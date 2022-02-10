680d8c40f95c7a1e71fd8b6b25084702690ff7b5
Alex Walker <amlwwalker@gmail.com>
2022-02-10 19:07:51 +0000

    Start of Ui reorder (objectView vs containerView)

--- frontend/src/components/layoutControlBar/index.jsx
+++ frontend/src/components/layoutControlBar/index.jsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import {createContainer} from "../../mocker/containers.js";
 
 function ControlBar({containers, onSelected, changeView, viewMode}) {
@@ -8,12 +8,14 @@ function ControlBar({containers, onSelected, changeView, viewMode}) {
             <div className="col-12">
                 <div className="molContainersHeader d-flex">
                     <div>
-                        <h2 className="atmContainerTitle">...</h2>
+                        <h2 className="atmContainerTitle">{/* if viewMode === 'x' show here. */}
+                           
+                        </h2>
                     </div>
                     <div className="ms-auto">
                         <button type="button" className={`atmButtonIcon ${viewMode ? "active" : "inactive"}`} onClick={()=>{changeView("grid")}}><i className="fas fa-th-large" /></button>
                         <button type="button" className={`atmButtonIcon ${viewMode ? "active" : "inactive"}`} onClick={()=>{changeView("list")}}><i className="fas fa-list" /></button>
-                        {/* <button type="button" className="" onClick={()=>{changeView("grid")}}><i className="fas fa-th-large" /></button> */}
+                        {/* <button type="button" className="atmButtonIcon" onClick={()=>{changeView("grid")}}><i className="fas fa-th-large" /></button> */}
                         {/* <button type="button" className="atmButtonIcon" onClick={()=>{changeView("list")}}><i className="fas fa-list" /></button> */}
                         <button type="button" className="atmButtonSimple" onClick={async () => createContainer("my container")}><i className="fas fa-plus-circle"/>New container</button>
                     </div>
