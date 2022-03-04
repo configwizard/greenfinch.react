13ab326deace741871acf44ef5c6fc309285de88
Alex Walker <amlwwalker@gmail.com>
2022-03-04 19:34:43 +0000

    keep up-to-date

--- frontend/src/components/compOverlayMenu/index.jsx
+++ frontend/src/components/compOverlayMenu/index.jsx
@@ -19,14 +19,14 @@ function CompOverlayMenu(props) {
                             </>
                         : null }
                         <button data-bs-toggle="modal" data-bs-target="#deleteModal" type="button" className="atmButtonBase nav-link"><i className="fas fa-trash-alt"/>&nbsp;Delete</button>
+                        <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <CompModal title="Delete?" buttonTextPrimary="Yes" buttonTextSecondary="No" clicked={props.onDelete}>
+                                <Form.Label>Are you sure?</Form.Label>
+                                <Form.Text>Are you sure you want to delete this item?</Form.Text>
+                            </CompModal>
+                        </div>
                     </nav>
                     {/* <button onClick={async () => {console.log("click close"); await this.props.setShow(false)}} className="atmButtonIcon">Close</button> */}
-                </div>
-                <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <CompModal title="Delete?" buttonTextPrimary="Yes" buttonTextSecondary="No" clicked={props.onDelete}>
-                        <Form.Label>Are you sure?</Form.Label>
-                        <Form.Text>Are you sure you want to delete this item?</Form.Text>
-                    </CompModal>
                 </div>
             </div> : null
     )
