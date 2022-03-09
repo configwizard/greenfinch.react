5a4912c3a0316c2f54525d5f9b9d14080eb62a71
Robin Green <robin@designingreen.co.uk>
2022-03-09 19:13:48 +0000

    reposition the progress bar

--- frontend/src/components/compProgress/index.jsx
+++ frontend/src/components/compProgress/index.jsx
@@ -15,10 +15,7 @@ export default class CompProgress extends React.Component {
             show: false
         }
     }
-
-    /* One to check:
-        https://getbootstrap.com/docs/5.0/components/progress/
-    */
+    /* One to check: https://getbootstrap.com/docs/5.0/components/progress/ */
      async componentDidMount () {
         console.log("percentage ready")
         window.runtime.EventsOn(name, async (progressMessage) => {
@@ -33,27 +30,30 @@ export default class CompProgress extends React.Component {
             }
         })
     }
-
     render() {
         if (!this.state.show) {
             return null
         }
         return (
-            <div className="molProgress utTemp d-flex align-items-center justify-content-center">
-                <div className="molProgressContainer">
-                    <h4>{this.state.title}</h4>
-                    <Progress
-                        theme={{
-                            active: {
-                                color: 'rgba(0, 175, 75, 0.5)'
-                            },
-                            success: {
-                                color: 'rgb(0, 175, 75)'
-                            }
-                        }}
-                        percent={this.state.percentage}/>
-                    <div className="molProgressFooter">
-                        <button onClick={async () => {await this.setState({...this.state, show: false})}} className="atmButtonIcon">Close</button>
+            <div className="molProgressContainer">
+                <div className="molProgressWrapper">
+                    <div className="molProgress d-flex align-items-center justify-content-center">
+                        <div className="molProgressBody">
+                            <h4>{this.state.title}</h4>
+                            <Progress
+                                theme={{
+                                    active: {
+                                        color: 'rgba(0, 175, 75, 0.5)'
+                                    },
+                                    success: {
+                                        color: 'rgb(0, 175, 75)'
+                                    }
+                                }}
+                                percent={this.state.percentage}/>
+                        </div>
+                        <div className="molProgressFooter">
+                            <button onClick={async () => {await this.setState({...this.state, show: false})}} className="atmButtonIcon">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/layoutFooter/index.jsx
+++ frontend/src/components/layoutFooter/index.jsx
@@ -3,7 +3,6 @@ import FadeProps from 'fade-props';
 import CompModal from "../compModal";
 import CompProgress from "../compProgress";
 
-
 export default function Footer({fireToast, percentage}) {
     const [show, setShow] = useState(false)
     return (
@@ -11,14 +10,14 @@ export default function Footer({fireToast, percentage}) {
             <div className="d-flex ms-auto">
                 <button type="button" className="atmButtonSimple" data-bs-toggle="modal" data-bs-target="#exampleModal"><i className="fas fa-hand-point-right"/>Click me for modal</button>
                 <button type="button" className="atmButtonSimple" onClick={() => setShow(true)}><i className="fas fa-hand-point-right"/>Click me for progress</button>
-                <button type="button" className="atmButtonSimple" onClick={() => {fireToast({Title: "clicked", Type:"warning", Description:"Clicking toast works!"})}}><i className="fas fa-hand-point-right"/>Click me for Toast (success)</button>
+                <button type="button" className="atmButtonSimple" onClick={() => {fireToast({Title: "clicked", Type:"success", Description:"Toast launched."})}}><i className="fas fa-hand-point-right"/>Click me for Toast (success)</button>
             </div>
             <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <CompModal title="Greenfinch Modal" buttonTextPrimary="OK" buttonTextSecondary="Cancel">
                     <p>Here is the content for the modal. I will need styling.</p>
                 </CompModal>
             </div>
-            <div>
+            <div style={{"position":"relative"}}>
                 <FadeProps animationLength={150}>
                     <CompProgress show={show} setShow={setShow} percentage={percentage}></CompProgress>
                 </FadeProps>
