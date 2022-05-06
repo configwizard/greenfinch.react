644a24649c00ad56cd49225e8fb4e7c0910dfcfc
Robin Green <robin@designingreen.co.uk>
2022-05-06 16:34:58 +0100

    Clean up settings drawer

--- frontend/src/components/organisms/Drawer/DrawerSettings/index.jsx
+++ frontend/src/components/organisms/Drawer/DrawerSettings/index.jsx
@@ -18,7 +18,7 @@ const DrawerSettings = (props) => {
             <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
                 <div className="offcanvas-header d-flex align-items-center">
                     <h4 id="offcanvasRightLabel"><i className="fas fa-lg fa-cog"/>&nbsp;Account Settings</h4>
-                    <button type="button" className="atmButtonIconClean" data-bs-dismiss="offcanvas" aria-label="Close"><i className="fas fa-times"/></button>
+                    <button type="button" className="button-offcanvas" data-bs-dismiss="offcanvas" aria-label="Close"><i className="fas fa-times"/></button>
                 </div>
                 <div className="offcanvas-body">
                     <div className="molBlockDropdown d-flex">
@@ -37,26 +37,31 @@ const DrawerSettings = (props) => {
                     <div className="molBlockSwitch d-flex">
                         <div className="atmSwitchContent">
                             <h5>Network</h5>
-                            <p>Select testNet or mainNet (disabled for hackathon and will only connect to testNet</p>
+                            <p>Testnet</p>
+                            <p className="temp-small">N.B. Testnet/Mainnet disabled for hackathon.</p>
                         </div>
-                        <div className="atmSwitchToggle ms-auto">
-                            <label className="switch">
-                                <input type="checkbox"/>
-                                <span className="slider round"></span>
-                            </label>
-                        </div>
+                        {/*
+                            <div className="atmSwitchToggle ms-auto">
+                                <label className="switch">
+                                    <input type="checkbox"/>
+                                    <span className="slider round"></span>
+                                </label>
+                            </div>
+                        */}
                     </div>
                     <div className="molBlockSwitch d-flex">
                         <div className="atmSwitchContent">
                             <h5>Local server API</h5>
-                            <p>Expose locally public read containers content. This allows other applications to access your public objects.<br />To access it, visit <b onClick={() => openInDefaultBrowser("http://localhost:43520/api/v1/readonly?since=0")}>http://localhost:43520/api/v1/readonly?since=0</b>,<br />where <pre>since=...</pre> can be used to filter objects by a unix timestamp (in seconds)</p>
+                            <p className="temp-small">Expose locally public read containers content. This allows other applications to access your public objects. To access it, visit <button onClick={() => openInDefaultBrowser("http://localhost:43520/api/v1/readonly?since=0")}>http://localhost:43520/api/v1/readonly?since=0</button>,<br />where <b>since=...</b> can be used to filter objects by a unix timestamp (in seconds).</p>
                         </div>
+                        {/*
                         <div className="atmSwitchToggle ms-auto">
                             <label className="switch">
                                 <input type="checkbox"/>
                                 <span className="slider round"></span>
                             </label>
                         </div>
+                        */}
                     </div>
                     <div className="molBlockSwitch d-flex">
                         <div className="atmSwitchContent">
