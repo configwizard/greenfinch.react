d7168bd3ed613c581bc534fba6345500c5a3684d
Robin Green <robin@designingreen.co.uk>
2022-05-09 10:50:07 +0100

    Homepage message added

--- frontend/src/components/templates/Home/index.jsx
+++ frontend/src/components/templates/Home/index.jsx
@@ -6,6 +6,8 @@ import HeaderPage from '../../organisms/HeaderPage';
 import LoadWallet from '../../organisms/LoadWallet';
 import { SectionHomepage, SectionSupport } from '../../organisms/HomeSections';
 
+import {openInDefaultBrowser} from "../../../manager/manager";
+
 // Central style sheet for templates
 import '../_settings/style.scss';
 
@@ -27,6 +29,21 @@ const TemplateHome = ({ account, recentWallets, refreshRecentWallets }) => {
                                     <SectionSupport
                                         titleLevel={"h3"}
                                         sectionTitle={"Help and support"} />
+                                    <div>
+                                        <HeadingGeneral 
+                                            level={"h3"}
+                                            isUppercase={false}
+                                            text={"Create new wallet"} />
+                                        <ol className="home-ol">
+                                            <li>Click &lsquo;Create new wallet&rsquo;</li>
+                                            <li>Copy your wallet address (starting with 'N')</li>
+                                            <li>Go to the <button onClick={() => openInDefaultBrowser("https://neowish.ngd.network/#/")}>N3 TestNet Faucet</button></li>
+                                            <li>Input your N3 address</li>
+                                            <li>Verify account via GitHub and ReCaptcha</li>
+                                            <li>In Greenfinch, open your wallet (<i className="fal fa-wallet"/>), transfer some GAS to NeoFS</li>
+                                        </ol>
+                                        <p className="home-sm">N.B. The N3 TestNet Faucet page provides 50 NEO and 50 GAS from testnet for one NEO address and one IP address every 24 hours.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
