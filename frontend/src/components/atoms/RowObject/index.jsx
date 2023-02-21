742a904d19d32799a0850e5a94e2fb03eba31dc8
Robin Green <robin@designingreen.co.uk>
2023-02-21 19:01:10 +0000

    Add reacvt-middle-ellipsis

--- frontend/src/components/atoms/RowObject/index.jsx
+++ frontend/src/components/atoms/RowObject/index.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { fileSize } from 'humanize-plus';
 
+import MiddleEllipsis from 'react-middle-ellipsis';
+
 import './style.scss';
 
 const RowObject = ({ onClick, dataType, objectFile, objectName, objectSize, objectOrigin }) => {
@@ -18,8 +20,10 @@ const RowObject = ({ onClick, dataType, objectFile, objectName, objectSize, obje
                     <div class="imagewrapper d-flex align-items-center">
                         { objectFile ? <figure className="atmRowFile d-flex align-items-center justify-content-center"><img className="mw-100 mh-100" src={`data:image/png;base64,${objectFile}`} alt={objectName} /></figure> : <i className="fas fa-folder" data-type={dataType}/> }
                     </div>
-                    <div>    
-                        <span className="atmButtonRowName">{objectName}</span>
+                    <div className="setwidth">
+                        <MiddleEllipsis>
+                            <span className="atmButtonRowName">{objectName}</span>
+                        </MiddleEllipsis>
                     </div>
                     <div>{fileSize(objectSize)}</div>
                     <div><Moment unix format="DD MMM YY">{objectOrigin}</Moment></div>
