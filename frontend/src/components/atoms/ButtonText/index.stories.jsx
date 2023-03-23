efaa65fa97d10f513b1ac816d46237a5f5c8e333
Robin Green <robin@designingreen.co.uk>
2023-03-23 19:28:55 +0000

    Work on component stories

--- frontend/src/components/atoms/ButtonText/index.stories.jsx
+++ frontend/src/components/atoms/ButtonText/index.stories.jsx
@@ -2,45 +2,70 @@
 import React from 'react';
 import ButtonText from './index';
 
+import './style.scss';
+
 // This default export determines where your story goes in the story list
 export default {
     title: 'Greenfinch/Button Text',
     component: ButtonText,
+    parameters: {
+        docs: {
+            description: {
+                component: 'Standard _Greenfinch_ text button.',
+            },
+        },
+    },
+    argTypes: {
+        onClick: {
+            description: 'Overwritten description',
+            table: {
+                type: {
+                    summary: 'Something short',
+                    detail: 'Something really really long',
+                },
+            },
+            control: {
+                type: null,
+            },
+        },
+    }
 };
 
 // We create a “template” of how args map to rendering
 const Template = (args) => <ButtonText {...args} />; 
 
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
+// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+// https://storybook.js.org/docs/react/writing-docs/doc-block-argstable
+// https://storybook.js.org/addons/@storybook/addon-docs
 
 export const Default = Template.bind({});
 Default.args = {
     type: 'default',
-    text: 'Button - default',
-    hasIcon: true,
-    faClass: "fa-sharp fa-solid fa-flag",
+    text: 'Button Text - default',
 };
 
 export const Clean = Template.bind({});
 Clean.args = {
     type: 'clean',
-    text: 'Button - clean',
-    hasIcon: true,
-    faClass: "fa-sharp fa-solid fa-flag",
+    text: 'Button Text - clean',
 };
 
 export const Primary = Template.bind({});
 Primary.args = {
     type: 'primary',
-    text: 'Button - primary',
-    hasIcon: true,
-    faClass: "fa-sharp fa-solid fa-flag",
+    text: 'Button Text - primary',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
     type: 'secondary',
-    text: 'Button - secondary',
-    hasIcon: true,
-    faClass: "fa-sharp fa-solid fa-flag",
+    text: 'Button Text - secondary',
+};
+
+export const Dora = Template.bind({});
+Dora.args = {
+    type: 'dora',
+    text: 'View on Dora',
+    faClass: 'fak fa-doracoz'
 };
